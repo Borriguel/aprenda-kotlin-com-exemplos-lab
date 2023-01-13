0013b937a9382efa10beaaeba0364c8f5d45e78c
Borriguel <rodolpho-henrique@hotmail.com.br>
2023-01-12 21:24:49 -0300

    solução do desafio proposto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,41 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { FACIL, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nomeUsuario: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+        usuarios.forEach { println("${it.nomeUsuario} matriculado com sucesso em $nome") }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //  Criação de usuários
+    val tobia = Usuario("Tobia", "tobia@gmail.com")
+    val jacare = Usuario("Jacaré", "jacare@dio.com")
+    val derp = Usuario("Derp", "derp@meme.com")
+
+    // Criação de conteúdos educacionais
+    val java = ConteudoEducacional("Java OO", 180, Nivel.INTERMEDIARIO)
+    val kotlin = ConteudoEducacional("Kotlin Completo + API Rest", 240, Nivel.DIFICIL)
+    val mysql = ConteudoEducacional("Comandos Básicos MySQL", 60, Nivel.FACIL)
+
+    // Criação Formação
+    val backend = Formacao("Formação Backend de sucesso!", listOf(java, kotlin, mysql))
+
+    // Teste do método matricular()
+    backend.matricular(tobia, jacare)
+    backend.matricular(derp)
+
+    println("Imprimindo inscritos no Formação Backend de sucesso!")
+    backend.inscritos.forEach { println(it.nomeUsuario) }
+
+    println(backend)
 }
